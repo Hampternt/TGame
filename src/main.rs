1777a4fe0c23ed9fdcaaf1da4bc98172d790815f
Hampternt <jesperlovland@proton.me>
2025-05-12 03:08:10 +0200

    moving import to lib

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-pub mod lib {
-    pub mod data_types;
-}
-pub use lib::data_types::CharacterStats;
-
+pub use TGame::lib::data_types::CharacterStats;
 fn main() {
     println!("Hello, world!");
-    let mut _player1 = CharacterStats::default();
+    let mut _player1 = CharacterStats::default(); 
 }
