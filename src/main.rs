19f8c0fef1ced276caa373e69802f1cd5423cec6
Hampternt <jesperlovland@proton.me>
2025-05-12 19:03:13 +0200

    added name test

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ pub use TGame::lib::data_types::CharacterStats;
 fn main() {
     println!("Hello, world!");
     let mut _player1 = CharacterStats::default(); 
+    println!("{} is name yes", _player1.name());
 }
