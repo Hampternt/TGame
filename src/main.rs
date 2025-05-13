e2a2db91614d73f4a41ecf4b792f865bd544fa4e
Hampternt <jesperlovland@proton.me>
2025-05-13 04:54:33 +0200

    some fixes on name to camel case and test
    some fun testing on the struct i created and the get setters.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-pub use TGame::lib::data_types::CharacterStats;
+pub use text_game::lib::data_types::CharacterStats;
 fn main() {
     println!("Hello, world!");
-    let mut _player1 = CharacterStats::default(); 
-    println!("{} is name yes", _player1.name());
+    let mut player1 = CharacterStats::default(); 
+    println!("{} is name yes", player1.name());
+
+    player1.set_name("Hammy".to_string());
+    
+    println!("{} is now mah nem.", player1.name());
 }
